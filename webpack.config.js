0650c500d532df07a983b42c807826bc41c800aa
Ss098751900Ss <ss098751900ss@gmail.com>
2023-03-01 11:54:54 +0900

    third commits

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
 // webpack.config.js
 
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "build"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+    port: 3000,
   },
 };
