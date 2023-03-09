e939d880929786d83c25cdc7a267cdd9b1b4e3b6
Ss098751900Ss <ss098751900ss@gmail.com>
2023-03-09 18:46:45 +0900

    typescriptのコンストラクトパラメーターっていう式の評価方法についてわからないところがあるがもう寝るのでセーブ

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,55 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import awsconfig from "./aws-exports";
 import Map from "react-map-gl";
 import "@aws-amplify/ui-react/styles.css";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { AppContext, defaultState } from "./AppContext";
 import { useControl } from "react-map-gl";
-import { useCallback, useState } from "react";
+import { useCallback, useState, useContext } from "react";
 import MapboxDraw from "@mapbox/mapbox-gl-draw";
 
-import { MapRef } from "react-map-gl";
+import type { MapRef, ControlPosition } from "react-map-gl";
 
 Amplify.configure(awsconfig);
 
-const DrawControl = () => {
+type DrawControlProps = ConstructorParameters<typeof MapboxDraw>[0] & {
+  position?: ControlPosition;
+
+  onCreate?: (evt: { features: object[] }) => void;
+  onUpdate?: (evt: { features: object[]; action: string }) => void;
+  onDelete?: (evt: { features: object[] }) => void;
+};
+
+function DrawControl(props: DrawControlProps): FunctionComponent {
   const [features, setFeatures] = useState({});
-  const onUpdate = useCallback((e) => {
+  const context = useContext(AppContext);
+
+  useControl<MapboxDraw>(
+    () => new MapboxDraw(props),
+    ({ map }: { map: MapRef }) => {
+      map.on("draw.create", props.onUpdate);
+      map.on("draw.update", props.onUpdate);
+      map.on("draw.delete", props.onDelete);
+    },
+    ({ map }: { map: MapRef }) => {
+      map.off("draw.create", props.onUpdate);
+      map.off("draw.update", props.onUpdate);
+      map.off("draw.delete", props.onDelete);
+    },
+    {
+      position: context.props.position,
+    }
+  );
+
+  return null;
+}
+
+function App() {
+  const onUpdate = useCallback((e: any) => {
     setFeatures((currFeatures) => {
-      const newFeatures = { ...currFeatures };
+      const newFeatures: any = { ...currFeatures };
       for (const f of e.features) {
         newFeatures[f.id] = f;
       }
@@ -25,9 +57,9 @@ const DrawControl = () => {
     });
   }, []);
 
-  const onDelete = useCallback((e) => {
+  const onDelete = useCallback((e: any) => {
     setFeatures((currFeatures) => {
-      const newFeatures = { ...currFeatures };
+      const newFeatures: any = { ...currFeatures };
       for (const f of e.features) {
         delete newFeatures[f.id];
       }
@@ -35,69 +67,70 @@ const DrawControl = () => {
     });
   }, []);
 
-  useControl<MapboxDraw>(
-    () => new MapboxDraw(props),
-    ({ map }: { map: MapRef }) => {
-      map.on("draw.create", onUpdate);
-      map.on("draw.update", onUpdate);
-      map.on("draw.delete", onDelete);
-    },
-    ({ map }: { map: MapRef }) => {
-      map.off("draw.create", onUpdate);
-      map.off("draw.update", onUpdate);
-      map.off("draw.delete", onDelete);
-    },
-    {
-      position: props.position,
-    }
-  );
+  const hoge = typeof MapboxDraw;
+  console.log(hoge);
 
-  return null;
-};
-
-function App() {
   return (
     <>
-      <Map
-        initialViewState={{
-          longitude: -122.4,
-          latitude: 37.8,
-          zoom: 14,
+      <AppContext.Provide
+        value={{
+          map: defaultState.map,
+          props: defaultState.props,
+          state: defaultState.state,
+          config: {
+            locationClient: {},
+            transformRequest: defaultState.config.transformRequest,
+          },
+          hooks: {
+            useGeojson: {},
+            useDetectMode: {},
+          },
         }}
-        mapStyle="mapbox://styles/mapbox/navigation-night-v1"
-        style={{
-          width: "100vw",
-          height: "100vh",
-          zIndex: "0",
-          position: "absolute",
-          top: "0px",
-          left: "0px",
-        }}
-        mapboxAccessToken="pk.eyJ1Ijoic3MwOTg3NTE5MDBzcyIsImEiOiJjbDVyNjllejEyNGF2M2Jyb25zZzM4M2Y0In0.A4sZaXQPpyTCy5cWGm750w"
       >
-        <div
+        <Map
+          initialViewState={{
+            longitude: -122.4,
+            latitude: 37.8,
+            zoom: 14,
+          }}
+          mapStyle="mapbox://styles/mapbox/navigation-night-v1"
           style={{
+            width: "100vw",
+            height: "100vh",
+            zIndex: "0",
             position: "absolute",
-            top: "10px",
-            left: "10px",
-            zIndex: "1",
-            backgroundColor: "#25283a",
-            opacity: "0.8",
-            borderRadius: "10px",
-            padding: "10px",
+            top: "0px",
+            left: "0px",
           }}
+          mapboxAccessToken="pk.eyJ1Ijoic3MwOTg3NTE5MDBzcyIsImEiOiJjbDVyNjllejEyNGF2M2Jyb25zZzM4M2Y0In0.A4sZaXQPpyTCy5cWGm750w"
         >
-          <h1>This is map</h1>
-        </div>
-        <DrawControl
-          osition="top-right"
-          displayControlsDefault={false}
-          controls={{
-            polygon: true,
-            trash: true,
-          }}
-        />
-      </Map>
+          <div
+            style={{
+              position: "absolute",
+              top: "10px",
+              left: "10px",
+              zIndex: "1",
+              backgroundColor: "#25283a",
+              opacity: "0.8",
+              borderRadius: "10px",
+              padding: "10px",
+            }}
+          >
+            <h1>This is map</h1>
+          </div>
+          <DrawControl
+            position="top-right"
+            displayControlsDefault={false}
+            controls={{
+              polygon: true,
+              trash: true,
+            }}
+            onCreate={onUpdate}
+            onUpdate={onUpdate}
+            onDelete={onDelete}
+          />
+        </Map>
+      </AppContext.Provide>
     </>
   );
 }
